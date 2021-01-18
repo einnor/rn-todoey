713a51cb8e3827a1300af13e4ad02a05da432988
Ronnie Nyaga <ronnienyaga@gmail.com>
2021-01-18 12:19:24 +0300

    Style the components

--- components/Todo.js
+++ components/Todo.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Todo = ({ name }) => {
   return (
-    <View>
+    <View style={[styles.item, styles.align]}>
       <Text>{name}</Text>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: 'whitesmoke',
+  },
+  align: {
+    margin: 8,
+    padding: 8,
+  },
+});
 
 export default Todo;
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import Todo from './Todo';
 
 const TodoList = () => {
   return (
-    <View>
-      <Text>Todo List</Text>
+    <View style={styles.container}>
+      <Text style={[styles.align, styles.font]}>Todo List</Text>
       <Todo name="Milk the cow" />
       <Todo name="Herd the goats" />
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '80%',
+    marginBottom: 60,
+  },
+  align: {
+    alignSelf: 'center',
+  },
+  font: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
 
 export default TodoList;
