0b691a2a0ee23985736132bb21e4b1c2deda389c
Ronnie Nyaga <ronnienyaga@gmail.com>
2021-01-18 12:08:34 +0300

    Todo component

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Todo from './Todo';
+
 const TodoList = () => {
   return (
     <View>
       <Text>Todo List</Text>
+      <Todo name="Milk the cow" />
+      <Todo name="Herd the goats" />
     </View>
   )
 }
